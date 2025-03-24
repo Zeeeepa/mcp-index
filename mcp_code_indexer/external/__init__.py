2c78eee23812ce9e9b186bdb983e7a3fcc66429b
Zeeeepa <zeeeepa@gmail.com>
2025-03-24 02:48:07 +0000

    Delete mcp_code_indexer/external/__init__.py

--- mcp_code_indexer/external/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-External Dependencies Module
-
-This module provides a centralized interface for external dependencies.
-It allows for easier mocking and replacement of external components.
-"""
